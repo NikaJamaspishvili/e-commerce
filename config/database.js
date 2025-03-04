e7cea08b662b8477d14a36bd7218f6bc2ea17d0d
macbook <jamaspishvilinika@icloud.com>
2025-03-04 16:24:58 +0400

    commit

--- config/database.js
+++ config/database.js
@@ -2,15 +2,15 @@ import mysql from "mysql2/promise";
 import { unstable_cache } from "next/cache";
 
  export async function callDatabase(query,data) {
+    console.log('called the database');
     try {
-    
     const db = await mysql.createConnection({
     
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    Port: process.env.DB_PORT,
+    port: process.env.DB_PORT,
     ssl:{
         mode:'REQUIRED',
         ca: process.env.DB_CA,
